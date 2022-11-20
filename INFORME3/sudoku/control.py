33499d17afdbcb4398d60ce8fc451e0b242d920e
AlejoPatigno <alpatinob@unal.edu.co>
2022-11-20 10:57:05 -0500

    completo

--- INFORME3/sudoku/control.py
+++ INFORME3/sudoku/control.py
@@ -5,11 +5,12 @@ interfaz.explicarJuego()
 tableroJuego = logica.obtenerTableroLogico()
 tableroJuego = interfaz.crearPistas(tableroJuego)
 posibleGanador = logica.determinarSiGano(tableroJuego)
+pistasIniciales = tableroJuego
 
 while posibleGanador == False:
     posicion = int(input("   Ingrese posicion a llenar: "))
     caracter = int(input("ingrese el numero con el que llenar la posicion:  "))
-    if logica.determinarError(tableroJuego,posicion, caracter):
+    if logica.determinarError(tableroJuego,posicion, caracter) and (posicion in logica.tableroFaltante(pistasIniciales)):
         tableroJuego = logica.actualizarTableroLogico(tableroJuego, posicion, caracter)
     else: print("la posicion ingresada no es valida")
     interfaz.dibujarTablero(tableroJuego)
